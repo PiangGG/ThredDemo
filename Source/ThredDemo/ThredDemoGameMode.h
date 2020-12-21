f0eedfcf39279deaedb39b922ed0b9ea82bbd765
PiangGG <piangpiangpiang@outlook.com>
2020-12-21 17:20:53 +0800

    commit

---
+++ Source/ThredDemo/ThredDemoGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ThredDemoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AThredDemoGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AThredDemoGameMode();
+};
+
+
+
